c57bf57fd3c1b0ade8968d74f725a8b71a16d5ff
Steven Rowland <steven.rowland@wework.com>
2020-02-15 15:24:52 -0500

    initial commit of goood stuff

---
+++ src/hypermodern/console.py
@@ -0,0 +1,11 @@
+# src/hyppermodern/console.py
+import click
+
+from . import __version__
+
+@click.command()
+@click.version_option(version=__version__)
+def main():
+    """The hypermodern Python project."""
+    click.echo("Hello, world!")
+    
