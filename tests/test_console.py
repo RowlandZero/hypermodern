9cfd8698cc3fb5f50db13e08dbdc526c847cbd66
rowlandzero <steven.rowland@wework.com>
2020-02-19 10:22:22 -0500

    mock

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,13 +4,16 @@ import pytest
 
 from hypermodern import console
 
+@pytest.fixture
+def mock_requests_get(mocker):
+    return mocker.patch("requests.get")
 
 @pytest.fixture
 def runner():
     return click.testing.CliRunner()
 
 
-def test_main_succeeds():
+def test_main_succeeds(runner, mock_requests_get):
     runner  = click.testing.CliRunner()
     result = runner.invoke(console.main)
     assert result.exit_code == 0
