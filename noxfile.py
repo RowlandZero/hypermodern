aaa90ea5073401fe9361da378fec30a28b42347d
rowlandzero <steven.rowland@wework.com>
2020-02-24 12:30:16 -0500

    gogo

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,18 @@
 # noxfile.py
 import nox
 
+locations = "src", "tests", "noxfile.py"
 
-@nox.session(python=["3.8", "3.7"])
+
+@nox.session(python=["3.8"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.8"])
 def tests(session):
-    args = session.posargs or ["--cov"]
+    args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
-    
